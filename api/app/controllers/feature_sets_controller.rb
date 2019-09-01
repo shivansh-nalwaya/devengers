8f275a9e2146cbbf0b2169130d536f5e2a2b0142
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2019-09-01 10:35:11 +0530

    Beautifuler charts

--- api/app/controllers/feature_sets_controller.rb
+++ api/app/controllers/feature_sets_controller.rb
@@ -6,28 +6,28 @@ class FeatureSetsController < ApplicationController
   protect_from_forgery only: %i[create update destroy]
 
   def index
-    @feature_sets = FeatureSet.stats
-    render json: { message: "Loaded feature_sets", feature_sets: @feature_sets }, status: :ok
+    feature_sets = FeatureSet.stats
+    render json: { message: "Loaded feature_sets", feature_sets: feature_sets }, status: :ok
   end
 
   def show
-    @feature_set = FeatureSet.find_by(id: params[:id])
-    render json: { message: "Loaded feature_set", feature_set: @feature_set }, status: :ok
+    feature_set = FeatureSet.find_by(id: params[:id])
+    render json: { message: "Loaded feature_set", feature_set: feature_set }, status: :ok
   end
 
   def create
-    @feature_set = FeatureSet.create(feature_set_params)
+    feature_set = FeatureSet.create(feature_set_params)
     uri = URI("http://trailblazers.southeastasia.cloudapp.azure.com:4444/predict")
     http = Net::HTTP.new(uri.host, uri.port)
     req = Net::HTTP::Post.new(uri.path, { "Content-Type" => "application/json" })
-    req.body = { "data" => [@feature_set.data.values] }.to_json
+    req.body = { "data" => [feature_set.data.values] }.to_json
     res = http.request(req)
     resp = JSON.parse(res.body)[0]
-    @feature_set.data["treatment_required"] = resp == 0 ? "No" : "Yes"
-    if @feature_set.save
-      render json: { message: "Created feature_set", feature_set: @feature_set, resp: resp }, status: :ok
+    feature_set.data["treatment_required"] = resp == 0 ? "No" : "Yes"
+    if feature_set.save
+      render json: { message: "Created feature_set", feature_set: feature_set, resp: resp }, status: :ok
     else
-      render json: { message: @feature_set.errors }, status: :unprocessable_entity
+      render json: { message: feature_set.errors }, status: :unprocessable_entity
     end
   end
 
@@ -54,17 +54,17 @@ class FeatureSetsController < ApplicationController
   end
 
   def update
-    @feature_set = FeatureSet.find_by(id: params[:id])
-    if @feature_set.update(feature_set_params)
-      render json: { message: "Updated feature_set", feature_set: @feature_set }, status: :ok
+    feature_set = FeatureSet.find_by(id: params[:id])
+    if feature_set.update(feature_set_params)
+      render json: { message: "Updated feature_set", feature_set: feature_set }, status: :ok
     else
-      render json: { message: @feature_set.errors }, status: :unprocessable_entity
+      render json: { message: feature_set.errors }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @feature_set = FeatureSet.find_by(id: params[:id])
-    @feature_set.destroy
+    feature_set = FeatureSet.find_by(id: params[:id])
+    feature_set.destroy
   end
 
   private
